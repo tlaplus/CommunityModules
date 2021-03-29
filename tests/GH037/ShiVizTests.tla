tlaplus/CommunityModules

commit a39cb8af01febb10a227e2f5adb9264a2f394c7e

Convert EXTENDS to LOCAL INSTANCE to minimize name clashes.
[Refactor]

modules/ShiViz.tla
@@ -1,5 +1,8 @@
 ------------------------------- MODULE ShiViz -------------------------------
-EXTENDS Integers, Json, Toolbox, TLC
+LOCAL INSTANCE Integers
+LOCAL INSTANCE Json
+LOCAL INSTANCE Toolbox
+LOCAL INSTANCE TLC
 
 -----------------------------------------------------------------------------
 

modules/FiniteSetsExt.tla
@@ -1,5 +1,7 @@
 --------------------------- MODULE FiniteSetsExt ---------------------------
-EXTENDS Naturals, FiniteSets, Functions
+LOCAL INSTANCE Naturals
+LOCAL INSTANCE FiniteSets
+LOCAL INSTANCE Functions
 
 
 ReduceSet(op(_, _), set, acc) ==

modules/Relation.tla
@@ -1,5 +1,6 @@
 ----------------------------- MODULE Relation ------------------------------
-EXTENDS Naturals, FiniteSets
+LOCAL INSTANCE Naturals
+LOCAL INSTANCE FiniteSets
 
 (***************************************************************************)
 (* This module provides some basic operations on relations, represented    *)

modules/SVG.tla
@@ -1,5 +1,9 @@
 ----------------------------- MODULE SVG -----------------------------
-EXTENDS Naturals, Sequences, Integers, TLC, FiniteSets
+LOCAL INSTANCE Naturals
+LOCAL INSTANCE Sequences
+LOCAL INSTANCE Integers
+LOCAL INSTANCE TLC
+LOCAL INSTANCE FiniteSets
 
 (******************************************************************************)
 (* Helper Operators                                                           *)

tests/FiniteSetsExtTests.tla
@@ -4,7 +4,7 @@ NEXT Next
 =====
 
 ------------------------- MODULE FiniteSetsExtTests -------------------------
-EXTENDS Integers, TLC, TLCExt, FiniteSetsExt
+EXTENDS Integers, TLC, TLCExt, FiniteSets, FiniteSetsExt
 
 ASSUME LET S == {"a","b","c","c"}
        IN Quantify(S, LAMBDA s: s = "c") = Cardinality({s \in S : s = "c"})
@@ -46,15 +46,15 @@ ASSUME LET S == 1..27
 ASSUME ChooseUnique({2, 3, 4, 5}, LAMBDA x : x % 3 = 1) = 4
 
 ASSUME AssertError(
-           "Attempted to compute the value of an expression of form\nCHOOSE x \\in S: P, but no element of S satisfied P.\nline 94, col 26 to line 95, col 64 of module FiniteSetsExt", 
+           "Attempted to compute the value of an expression of form\nCHOOSE x \\in S: P, but no element of S satisfied P.\nline 96, col 26 to line 97, col 64 of module FiniteSetsExt", 
            ChooseUnique({2, 3, 4, 5}, LAMBDA x : TRUE))
 
 ASSUME AssertError(
-           "Attempted to compute the value of an expression of form\nCHOOSE x \\in S: P, but no element of S satisfied P.\nline 94, col 26 to line 95, col 64 of module FiniteSetsExt", 
+           "Attempted to compute the value of an expression of form\nCHOOSE x \\in S: P, but no element of S satisfied P.\nline 96, col 26 to line 97, col 64 of module FiniteSetsExt", 
            ChooseUnique({}, LAMBDA x : TRUE))
 
 ASSUME AssertError(
-           "Attempted to compute the value of an expression of form\nCHOOSE x \\in S: P, but no element of S satisfied P.\nline 94, col 26 to line 95, col 64 of module FiniteSetsExt", 
+           "Attempted to compute the value of an expression of form\nCHOOSE x \\in S: P, but no element of S satisfied P.\nline 96, col 26 to line 97, col 64 of module FiniteSetsExt", 
            ChooseUnique({2, 3, 4, 5}, LAMBDA x : FALSE))
 
 -----------------------------------------------------------------------------

commit 8f4f4ff7c1905709db4de6b2319e0bec760ea30d

Declaring instances local causes definition overrides to be hidden. In the case of Toolbox, this causes the definition override of `_TETrace` to be invisible.  In turn, TLC will then try to evaluate the TLA+ definition of `_TETrace` as defined in Tooblox.tla:
   Attempted to enumerate S \ T when S:
   Nat
   is not enumerable.

This is a regression caused by commit
a39cb8af01febb10a227e2f5adb9264a2f394c7e

Fixes Github issue #37
https://github.com/tlaplus/CommunityModules/issues/37

[Bug]

build.xml
@@ -117,6 +117,7 @@
 				<pathelement location="${dist}/CommunityModules-${timestamp}.jar" />
 			</classpath>
 		</java>
+		<!-- Above BFS, here simulation mode! -->
 		<java classname="tlc2.TLC" fork="true" failonerror="true">
 			<!-- Tell Java to use a garbage collector which makes TLC happy. -->
 			<jvmarg value="-XX:+UseParallelGC"/>
@@ -140,6 +141,41 @@
 				<pathelement location="${dist}/CommunityModules-${timestamp}.jar" />
 			</classpath>
 		</java>
+
+		<!-- Test if trace expressions involving the ShiViz module work correctly. -->
+		<!-- This test expects TLC to report a liveness violation, which is why    -->
+		<!-- failonerror is off.  Instead, the ant records TLC's return value in   -->
+		<!-- shiviz.return.value, which is cheked in the conditional below.        -->
+		<java classname="tlc2.TLC" fork="true" failonerror="false" resultproperty="shiviz.return.value">
+			<!-- Tell Java to use a garbage collector which makes TLC happy. -->
+			<jvmarg value="-XX:+UseParallelGC"/>
+
+			<!-- Report execution statistics as azure-pipeline -->
+			<sysproperty key="tlc2.TLC.ide" value="azure-pipeline"/>
+			<sysproperty key="util.ExecutionStatisticsCollector.id" value="01ed03e40ba44f278a934849dd2b1038"/>
+			<sysproperty key="TLA-Library" value="${src}"/>
+			
+			<arg value="-metadir"/>
+			<arg value="${basedir}/build/states"/>
+			<arg value="-cleanup"/>
+			<arg value="-deadlock"/>
+			<arg value="-noGenerateSpecTE"/>
+			<arg value="${basedir}/tests/GH037/ShiVizTests"/>
+
+			<classpath>
+				<pathelement location="${tlc}/tla2tools.jar" />
+				<!-- The jar that has just been built by the dist target. -->
+				<pathelement location="${dist}/CommunityModules-${timestamp}.jar" />
+			</classpath>
+		</java>
+	    <fail message="ERROR: ShiViz test failed">
+	        <condition>
+	            <not>
+	            	<!-- Assert that TLC's error code is 13, which is the code corresponding to a liveness violation. -->
+	                <equals arg1="${shiviz.return.value}" arg2="13"/>
+	            </not>
+	        </condition>
+	    </fail>
 
 		<junit dir="${build}" printsummary="yes" haltonfailure="yes" showoutput="no" haltonerror="yes"  forkmode="perTest" fork="yes">
 			<!-- enable all assertions -->

modules/ShiViz.tla
@@ -1,8 +1,19 @@
 ------------------------------- MODULE ShiViz -------------------------------
 LOCAL INSTANCE Integers
 LOCAL INSTANCE Json
-LOCAL INSTANCE Toolbox
 LOCAL INSTANCE TLC
+
+\* Declaring instances local causes definition overrides to be hidden. In the
+\* case of Toolbox, this causes the definition override of `_TETrace` to be
+\* invisible.  In turn, TLC will then try to evaluate the TLA+ definition of
+\*
+\* `_TETrace` as defined in Tooblox.tla:
+\*   Attempted to enumerate S \ T when S:
+\*   Nat
+\*   is not enumerable.
+\*
+\* See: https://github.com/tlaplus/CommunityModules/issues/37
+INSTANCE Toolbox
 
 -----------------------------------------------------------------------------
 

tests/GH037/ShiVizTests.cfg
@@ -1,0 +1,28 @@
+\* MV CONSTANT declarations
+CONSTANTS
+ClientA = ClientA
+ClientB = ClientB
+\* MV CONSTANT definitions
+CONSTANT
+Clients <- const_161703600298862000
+\* CONSTANT declarations
+CONSTANT Database = Database
+\* CONSTANT declarations
+CONSTANT Cache = Cache
+CONSTANT
+_TETrace <- def_ov_161703600298863000
+\* INIT definition
+INIT
+init_161703600296659000
+\* NEXT definition
+NEXT
+next_161703600296660000
+\* Action Constraint definition
+\* ACTION_CONSTRAINT
+\* action_constr_161703600296661000
+\* PROPERTY definition
+PROPERTY
+_prop
+\* CHECK_DEADLOCK off because of PROPERTY or INVARIANT above.
+CHECK_DEADLOCK FALSE
+\* Generated on Mon Mar 29 09:40:02 PDT 2021

tests/GH037/ShiVizTests.tla
@@ -1,0 +1,215 @@
+\* :0:__trace_var_161703600296656000:Host"$!@$!@$!@$!@$!"
+\* :0:__trace_var_161703600296658000:_TETrace"$!@$!@$!@$!@$!"
+---- MODULE ShiVizTests ----
+EXTENDS TLC, ShiViz, Toolbox
+
+VARIABLES StorageValues, pc
+
+vars == << StorageValues, pc >>
+
+CONSTANTS Clients, Database, Cache
+
+Storages == {Database, Cache}
+
+\* MV CONSTANT declarations@modelParameterConstants
+CONSTANTS
+ClientA, ClientB
+----
+
+\* MV CONSTANT definitions Clients
+const_161703600298862000 == 
+{ClientA, ClientB}
+----
+
+def_ov_161703600298863000 == 
+    <<
+    ([StorageValues |-> (Database :> 0 @@ Cache :> 0),pc |-> (ClientA :> "WriteDatabase" @@ ClientB :> "WriteDatabase")]),
+    ([StorageValues |-> (Database :> ClientA @@ Cache :> 0),pc |-> (ClientA :> "WriteCache" @@ ClientB :> "WriteDatabase")]),
+    ([StorageValues |-> (Database :> ClientB @@ Cache :> 0),pc |-> (ClientA :> "WriteCache" @@ ClientB :> "WriteCache")]),
+    ([StorageValues |-> (Database :> ClientB @@ Cache :> ClientB),pc |-> (ClientA :> "WriteCache" @@ ClientB :> "Done")]),
+    ([StorageValues |-> (Database :> ClientB @@ Cache :> ClientA),pc |-> (ClientA :> "Done" @@ ClientB :> "Done")])
+    >>
+----
+
+
+\* TRACE EXPLORER variable declaration @traceExploreExpressions
+VARIABLES __trace_var_161703600296656000,__trace_var_161703600296658000
+----
+
+\* TRACE EXPLORER identifier definition @traceExploreExpressions:0
+trace_def_161703600296655000 ==
+Host
+----
+
+\* TRACE EXPLORER identifier definition @traceExploreExpressions:1
+trace_def_161703600296657000 ==
+_TETrace
+----
+
+\* TRACE Sub-Action definition 0
+next_sa_0 ==
+    (
+        /\ StorageValues = (
+                (Database :> 0 @@ Cache :> 0)
+            )
+        /\ pc = (
+                (ClientA :> "WriteDatabase" @@ ClientB :> "WriteDatabase")
+            )
+        /\ StorageValues' = (
+                (Database :> ClientA @@ Cache :> 0)
+            )
+        /\ pc' = (
+                (ClientA :> "WriteCache" @@ ClientB :> "WriteDatabase")
+            )
+        /\ __trace_var_161703600296656000' = (
+            trace_def_161703600296655000
+            )'
+        /\ __trace_var_161703600296658000' = (
+            trace_def_161703600296657000
+            )'
+    )
+
+\* TRACE Sub-Action definition 1
+next_sa_1 ==
+    (
+        /\ StorageValues = (
+                (Database :> ClientA @@ Cache :> 0)
+            )
+        /\ pc = (
+                (ClientA :> "WriteCache" @@ ClientB :> "WriteDatabase")
+            )
+        /\ StorageValues' = (
+                (Database :> ClientB @@ Cache :> 0)
+            )
+        /\ pc' = (
+                (ClientA :> "WriteCache" @@ ClientB :> "WriteCache")
+            )
+        /\ __trace_var_161703600296656000' = (
+            trace_def_161703600296655000
+            )'
+        /\ __trace_var_161703600296658000' = (
+            trace_def_161703600296657000
+            )'
+    )
+
+\* TRACE Sub-Action definition 2
+next_sa_2 ==
+    (
+        /\ StorageValues = (
+                (Database :> ClientB @@ Cache :> 0)
+            )
+        /\ pc = (
+                (ClientA :> "WriteCache" @@ ClientB :> "WriteCache")
+            )
+        /\ StorageValues' = (
+                (Database :> ClientB @@ Cache :> ClientB)
+            )
+        /\ pc' = (
+                (ClientA :> "WriteCache" @@ ClientB :> "Done")
+            )
+        /\ __trace_var_161703600296656000' = (
+            trace_def_161703600296655000
+            )'
+        /\ __trace_var_161703600296658000' = (
+            trace_def_161703600296657000
+            )'
+    )
+
+\* TRACE Sub-Action definition 3
+next_sa_3 ==
+    (
+        /\ StorageValues = (
+                (Database :> ClientB @@ Cache :> ClientB)
+            )
+        /\ pc = (
+                (ClientA :> "WriteCache" @@ ClientB :> "Done")
+            )
+        /\ StorageValues' = (
+                (Database :> ClientB @@ Cache :> ClientA)
+            )
+        /\ pc' = (
+                (ClientA :> "Done" @@ ClientB :> "Done")
+            )
+        /\ __trace_var_161703600296656000' = (
+            trace_def_161703600296655000
+            )'
+        /\ __trace_var_161703600296658000' = (
+            trace_def_161703600296657000
+            )'
+    )
+
+\* TRACE Sub-Action definition 4
+next_sa_4 ==
+    (
+        /\ StorageValues = (
+                (Database :> ClientB @@ Cache :> ClientA)
+            )
+        /\ pc = (
+                (ClientA :> "Done" @@ ClientB :> "Done")
+            )
+        /\ StorageValues' = (
+                (Database :> ClientB @@ Cache :> ClientA)
+            )
+        /\ pc' = (
+                (ClientA :> "Done" @@ ClientB :> "Done")
+            )
+        /\ __trace_var_161703600296656000' = (
+            trace_def_161703600296655000
+            )'
+        /\ __trace_var_161703600296658000' = (
+            trace_def_161703600296657000
+            )'
+    )
+
+\* TRACE Action Constraint definition traceExploreActionConstraint
+action_constr_161703600296661000 ==
+<<
+next_sa_0,
+next_sa_1,
+next_sa_2,
+next_sa_3,
+next_sa_4
+>>[TLCGet("level")]
+----
+
+\* TRACE INIT definition traceExploreInit
+init_161703600296659000 ==
+    /\ StorageValues = (
+            (Database :> 0 @@ Cache :> 0)
+        )
+    /\ pc = (
+            (ClientA :> "WriteDatabase" @@ ClientB :> "WriteDatabase")
+        )
+    /\ __trace_var_161703600296656000 = (
+            trace_def_161703600296655000
+        )
+    /\ __trace_var_161703600296658000 = (
+            trace_def_161703600296657000
+        )
+
+----
+
+\* TRACE NEXT definition traceExploreNext
+next_161703600296660000 ==
+    \/ next_sa_0
+    \/ next_sa_1
+    \/ next_sa_2
+    \/ next_sa_3
+    \/ next_sa_4
+
+
+\* PROPERTY definition
+_prop ==
+~<>[](
+    StorageValues = (
+    (Database :> ClientB @@ Cache :> ClientA)
+    )    /\
+    pc = (
+    (ClientA :> "Done" @@ ClientB :> "Done")
+    )
+)
+----
+
+=============================================================================
+\* Modification History
+\* Created Mon Mar 29 09:40:02 PDT 2021 by markus